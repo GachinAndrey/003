3ecb99c42c4b894242d7923e651fad92ec2da285
GachinAndrey <gachin.andrei@gmail.com>
2021-01-21 13:33:06 +0300

    Initial commit

---
+++ 01.py
@@ -0,0 +1,14 @@
+def my_func():
+    try:
+        number1 = float(input("Введите делимое: "))
+        number2 = float(input("Введите делитель: "))
+        resalt = number1 / number2
+    except ValueError:
+        return 'Введено не число!'
+    except ZeroDivisionError:
+        return "Вы не можете делить на ноль!"
+
+    return resalt
+
+
+print(f'Результат {my_func()}')
